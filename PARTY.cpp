b1b82bf64243cbca8ae661e654ac08509df26f37
deepcoder007 <vibhanshuprasad@gmail.com>
2014-05-05 17:31:38 +0530

    PARTY.cpp done :D

--- PARTY.cpp
+++ PARTY.cpp
@@ -2,6 +2,9 @@
 #include<cstdio>
 using namespace std;
 
+int a[101][501];
+int b[101][501];
+
 inline bool max( int a,int b)  //return true if b > a
 { return b>a; }
 
@@ -12,38 +15,40 @@ void test(int budget,int n)  // n is the no. of parties
 	int v[n+1];
 	int w[n+1];
 	//get inputs
-	for( i=0;i<n;i++)
+	for( i=0;i<n;i++)   //input data
 		cin>>w[i+1]>>v[i+1];
-	int* b[n+1];    //stores the maximum for corresponding coordinates
-	for(i=0;i<=n;i++)
-		b[i]=new int[budget+1];
 	for( i=0 ; i<=budget ; i++ )   //initialize the first trivial row
+	{
 		b[0][i]=0;
-	for( i=1 ; i<=n ; i++ )
+		a[0][i]=0;
+	}
+	for(i=0;i<=n;i++)
+	{
+		b[i][0]=0;
+		a[i][0]=0;
+	}
+	for( i=1 ; i<=n ; i++ )   // on the i items 
 	{
 		for( j=1 ;j <=budget ; j++ )
 		{
 			if( w[i] >  j ) //the element not accomodated in budget
 			{
 				b[i][j]=b[i-1][j];
+				a[i][j]=a[i-1][j];
 			}
 			else
 			{
-				if( max( b[i-1][j], b[i][j-w[i]] + v[i] ) ==true )  // i.e. true when new element should be included
+				if( max( b[i-1][j], b[i-1][j-w[i]] + v[i] ) ==true )  // i.e. true when new element should be included
 				{
-					b[i][j]=b[i][j-w[i]]+v[i];
-					if( b[i][j]>100 )
-					{
-					cout<<"Element included at: "<<i<<" "<<j<<" "<<b[i][j]<<endl;
-					fprintf(ptr,"AT : %d %d %d",i,j,b[i][j]);
-					}
+					b[i][j]=b[i-1][j-w[i]]+v[i];
+					a[i][j]=a[i-1][j-w[i]]+w[i];
 				}
 				else
-				{	b[i][j]=b[i-1][j];  }
+				{	b[i][j]=b[i-1][j];a[i][j]=a[i-1][j];  }
 			}
 		}
 	}
-	cout<<"Max fun: "<<b[n][budget]<<endl;
+	printf("%d %d\n",a[n][budget],b[n][budget]);
 	fclose(ptr);
 	return;
 }
