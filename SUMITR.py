cae589df19a4ce9bf1790ea6651e6fb5208fa7a1
deepcoder007 <vibhanshuprasad@gmail.com>
2014-05-20 22:42:03 +0530

    SUMTIR.py added

--- SUMITR.py
+++ SUMITR.py
@@ -1,16 +1,16 @@
-n=int(raw_input())
+n=int(input())
 for x in range(n):
-	l=int(raw_input())
+	l=int(input())
 	a=list()
 	for y in range(l):
-		a.append([int(c) for c in raw_input().split()])
-	y=l-2 
-	while y>=0:
+		a.append(map(int,input().split()))
+	l-=2
+	while l>=0:
 		z=0
-		while z<=y:
-			a[y][z]+=max(a[y+1][z],a[y+1][z+1])
+		while z<=l:
+			a[l][z]+=max(a[l+1][z],a[l+1][z+1])
 			z+=1
-		y-=1
-	print a[0][0]
+		l-=1
+	print(a[0][0])
 			
 
