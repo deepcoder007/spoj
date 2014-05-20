74cff130b581cb98067ba6033fd19d236fe08061
deepcoder007 <vibhanshuprasad@gmail.com>
2014-05-20 21:00:25 +0530

    SUMITR.py updated

--- SUMITR.py
+++ SUMITR.py
@@ -3,8 +3,14 @@ for x in range(n):
 	l=int(raw_input())
 	a=list()
 	for y in range(l):
-		a.append( [ int(c) for c in  raw_input().split() ] )
-	for y in range(l-1):
-		for z in range(y+1):
+		a.append([int(c) for c in raw_input().split()])
+	y=l-2 
+	while y>=0:
+		z=0
+		while z<=y:
+			a[y][z]+=max(a[y+1][z],a[y+1][z+1])
+			z+=1
+		y-=1
+	print a[0][0]
 			
 
